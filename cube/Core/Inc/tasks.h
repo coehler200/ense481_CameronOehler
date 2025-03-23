12a6edc785c46853057bf53151d8298de063dcdb
Cameron Oehler <coehler@hotmail.com>
2025-03-23 14:54:56 -0600

    Cube - Add tasks files

---
+++ cube/Core/Inc/tasks.h
@@ -0,0 +1,14 @@
+/*
+ * tasks.h
+ *
+ *  Created on: Mar 23, 2025
+ *      Author: coehl
+ */
+
+#ifndef INC_TASKS_H_
+#define INC_TASKS_H_
+
+void StartReadImu(void *argument);
+void StartCli(void *argument);
+
+#endif /* INC_TASKS_H_ */
---
+++ cube/Core/Src/tasks.c
@@ -0,0 +1,16 @@
+/*
+ * tasks.c
+ *
+ *  Created on: Mar 23, 2025
+ *      Author: coehl
+ */
+
+#include "tasks.h"
+
+void StartReadImu(void *argument){
+
+}
+
+void StartCli(void *argument){
+
+}
