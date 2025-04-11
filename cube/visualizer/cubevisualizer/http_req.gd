cd1983e6fb852ad21ee5c1abeb91b74cc10b9f18
Cameron Oehler <coehler@hotmail.com>
2025-04-11 11:57:43 -0600

    Cube - Add RotationOffset to visualizer

--- cube/visualizer/cubevisualizer/http_req.gd
+++ cube/visualizer/cubevisualizer/http_req.gd
@@ -1,16 +1,18 @@
 extends Node3D
 
 
+# Setup callback and make first request to flask server
 func _ready() -> void:
 	$HTTPRequest.request_completed.connect(_on_request_completed)
 	$HTTPRequest.request("http://127.0.0.1:8080")
 
 
+# On data received
 func _on_request_completed(result, response_code, headers, body):
-	var json = JSON.parse_string(body.get_string_from_utf8())
+	var json = JSON.parse_string(body.get_string_from_utf8()) # Parse response
 	var roll: float = json["r"]
 	var pitch: float = json["p"]
 	var heading: float = json["h"]
-	var orientation: Vector3 = Vector3(-pitch, heading, -roll)
+	var orientation: Vector3 = Vector3(-pitch, heading, -roll) # Set orientation of Node3D
 	rotation_degrees = orientation
 	$HTTPRequest.request("http://127.0.0.1:8080")
